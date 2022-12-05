5245ab33b072b035a899a389a3f1bd50dd7b41ef
Phil Vigus <phil.vigus@devoteam.com>
2022-12-05 15:58:33 +0100

    Add a basic test framework

---
+++ tests/unit/models/response.model.test.ts
@@ -0,0 +1,9 @@
+import ResponseModel from "../../../src/models/response.model";
+
+describe("Response model", () => {
+    it("Should do some funky stuff", () => {
+        const reponseModel = new ResponseModel();
+
+        expect(true).toEqual(true);
+    });
+});
