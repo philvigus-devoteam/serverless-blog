3f4b869f5dd977845708043a2fd01516f13e1cd5
Phil Vigus <phil.vigus@devoteam.com>
2022-12-12 13:30:40 +0100

    Fix body in response

--- src/functions/createArticle/handler.ts
+++ src/functions/createArticle/handler.ts
@@ -7,16 +7,15 @@ import { v4 as uuid } from 'uuid';
 const createArticle: Handler = async (event) => {
   const databaseService = new DatabaseService();
 
-  const body = Object.assign({}, JSON.parse(event.body));
-  body["id"] = uuid();
+  const body = { id: uuid(), ...JSON.parse(event.body) };
 
-  const article = await databaseService.create({
+  await databaseService.create({
     TableName: "articles",
     Item: body
   });
 
   return formatJSONResponse({
-    body: article,
+    body: body,
     event,
   });
 };
