dc43b4cd530df42c2659945b9a9cdf8b50d6694c
Phil Vigus <phil.vigus@devoteam.com>
2022-12-12 10:39:11 +0100

    Tidy up

--- src/functions/getArticleList/handler.ts
+++ src/functions/getArticleList/handler.ts
@@ -1,18 +1,12 @@
-import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { formatJSONResponse } from '@libs/api-gateway';
+import { Handler } from 'aws-lambda';
 import DatabaseService from 'src/services/database.service';
 
-const getArticleList: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
-  console.log('here');
+const getArticleList: Handler = async (event) => {
   const databaseService = new DatabaseService();
-  // await databaseService.create({TableName: "articles", Item: { "id": "a lovely value"}});
-  // await databaseService.create({TableName: "articles", Item: { "id": "an even lovelier one"}});
-  // await databaseService.create({TableName: "articles", Item: { "id": "the loveliest one"}});
 
-  console.log(databaseService);
   const articles = await databaseService.getAll({TableName: "articles"});
-  // const articles = await databaseService.get({TableName: "articles", Key: "wibble"});
-  console.log('there');
+
   return formatJSONResponse({
     body: articles,
     event,
